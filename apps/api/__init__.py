3401199ea921610377855d6d2334ee90af6999d3
jumpwill <707979910@qq.com>
2022-08-09 14:28:12 +0800

    修复init

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -3,9 +3,4 @@ from apps.api.v1 import router as v1_router
 
 router = APIRouter()
 
-<<<<<<< Updated upstream
-router.include_router(v1_router,prefix="/v1")
-=======
-
-router.include_router(auth_router,prefix="/auth")
->>>>>>> Stashed changes
+router.include_router(v1_router, prefix="/v1")
